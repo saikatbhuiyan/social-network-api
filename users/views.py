0a47e23edfbcf7f2a5c8ff63c7769758ef2bfc1c
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-04-06 15:26:37 +0600

    create login serializers and views

--- users/views.py
+++ users/views.py
@@ -2,14 +2,19 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import RegistrationSerializer
 
+
+from .serializers import (
+ LoginSerializer, RegistrationSerializer
+)
+from .renderers import UserJSONRenderer
 
 class RegistrationAPIView(APIView):
   # Allow any user (authenticated or not) to hit this endpoint.
   permission_classes = (AllowAny,)
+  renderer_classes = (UserJSONRenderer,)
   serializer_class = RegistrationSerializer
-  
+
   def post(self, request):
     user = request.data.get('user', {})
     # The create serializer, validate serializer, save serializer pattern
@@ -19,3 +24,19 @@ class RegistrationAPIView(APIView):
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data, status=status.HTTP_201_CREATED)
+  
+
+class LoginAPIView(APIView):
+  permission_classes = (AllowAny,)
+  renderer_classes = (UserJSONRenderer,)
+  serializer_class = LoginSerializer
+  
+  def post(self, request):
+    user = request.data.get('user', {})
+    # Notice here that we do not call `serializer.save()` like we did for
+    # the registration endpoint. This is because we don't have
+    # anything to save. Instead, the `validate` method on our serializer
+    # handles everything we need.
+    serializer = self.serializer_class(data=user)
+    serializer.is_valid(raise_exception=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
