e4bbfc829e51436c79c2395439dcef9bd3c5ac8b
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-04-07 23:59:06 +0600

    create view for articals

--- profiles/views.py
+++ profiles/views.py
@@ -1,29 +1,32 @@
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .models import Profile
 from .renderers import ProfileJSONRenderer
 from .serializers import ProfileSerializer
-from .exceptions import ProfileDoesNotExist
-
+# from .exceptions import ProfileDoesNotExist
 class ProfileRetrieveAPIView(RetrieveAPIView):
   permission_classes = (AllowAny,)
   renderer_classes = (ProfileJSONRenderer,)
   serializer_class = ProfileSerializer
+  queryset = Profile.objects.select_related('user')
   def retrieve(self, request, username, *args, **kwargs):
     # Try to retrieve the requested profile and throw an exception if the
     # profile could not be found.
     try:
-      # We use the `select_related` method to avoid making unnecessary
-      # database calls.
-      profile = Profile.objects.select_related('user').get(
-      user__username=username
-      )
+      # # We use the `select_related` method to avoid making unnecessary
+      # # database calls.
+      # profile = Profile.objects.select_related('user').get(
+      # user__username=username
+      # )
+      profile = self.queryset.get(user__username=username)
     except Profile.DoesNotExist:
-      raise ProfileDoesNotExist
-    
+      # raise ProfileDoesNotExist
+      raise NotFound('A profile with this username does not exist.')
+
     serializer = self.serializer_class(profile)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
